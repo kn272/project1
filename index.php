964bacbe4ce0a4533b4312544819399026d8d4fd
kishore kumar reddy nerkundram gajendran <kn272@afsconnect1.njit.edu>
2017-10-19 22:15:15 -0400

    basic working program

--- index.php
+++ index.php
@@ -53,16 +53,64 @@ abstract class page {
 
 class homepage extends page {
   public function get() {
-     $form = '<form action="index.php" method="post">';
+     $form = '<form action="index.php" method="post" enctype="multipart/form-data">';
      $form .= 'Upload .CSV file:<br>';
      $form .= '<input type="file" name="selectFile" id="selectFile">';
-     $form .= '<input type="submit" value="submit">';
+     $form .= '<input type="submit" value="submit" name="submit">';
      $form .= '</form>';
      $this->html .= '<h1>.CSV file upload</h1>';
      $this->html .= $form;
   }
+  
   public function post() {
+     $targetDir = "uploads/";
      print_r($_FILES);
+     $targetFile = $targetDir . $_FILES["selectFile"]["name"];
+     $fileType = pathinfo($targetFile,PATHINFO_EXTENSION);
+     if(isset($_POST["submit"])) {
+       $fileName=$_FILES["selectFile"]["tmp_name"];
+       // move_uploaded_file($_FILES["selectFile"]["tmp_name"], "uploads/" . $_FILES["selectFile"]["name"]);
+       move_uploaded_file($fileName,$targetFile);
+	echo 'File uploaded successfully.';
+	header('Location:?page=table&fileName='. $targetFile);
+     }
+  }
+}
+
+class table extends page {
+  public function get() {
+     $fileName = $_GET['fileName'];
+     echo trim($fileName, "uploads/"). " was uploaded successfully and table is as follows:<br><br>";
+     $heading = 1;
+     $handle = fopen($fileName,"r");
+     $table = '<table border="1">';
+     while(($data = fgetcsv($handle))!=FALSE) {
+        if ($heading == 1) {
+           $table .= '<thead><tr>';
+	   foreach ($data as $value) {
+              if(!isset($value))
+	         $value = "&nbsp";
+              else
+	         $table .= "<th>". $value ."</th>";
+	   }
+	   $table .=  '</tr></thead><tbody>';
+	}
+	else {
+           $table .= '<tr>';
+	   foreach ($data as $value) {
+              if(!isset($value)) 
+                 $value = "&nbsp";
+              else
+	         $table .=  "<td>". $value . "</td>";
+	   $table .= '</tr>';
+	   }
+	}
+    }
+    $heading++;
+    $table .= '</tbody></table>';
+    $this->html .= $table;
+    fclose($handle);
+    stringFunctions::printThis($this->html);
   }
 }
 
@@ -72,4 +120,5 @@ class stringFunctions {
   }
 }
 
-?>
+
+?>     
